81505bcbda0f41078fcedb453272ef6d4f3628e3
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-04-25 19:40:30 -0400

    fix

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from ast import And
-from urllib import response
-from flask import Flask, request, abort, url_for, redirect, render_template, session
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from dataclasses import dataclass
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' 
 app.config['SQLALCHEMY_BINDS'] = {'chat': 'sqlite:///chat.db'} 
 db = SQLAlchemy(app)
-
-
 
 class userChatter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,11 +18,10 @@ class userChatter(db.Model):
 
 class chatInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    chatMessage =  db.Column(db.String(150), nullable=False)
+    message =  db.Column(db.String(150), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    author  = db.Column(db.String(80), nullable=False)
     __bind_key__ = 'chat' 
-
-
 
 @app.route("/", methods=["GET", "POST"])
 def default():
@@ -39,7 +37,7 @@ def login_controller():
         if user and user.password == password:
             return redirect(url_for("profile", username=username))
         else:
-            return render_template("loginPage.html", error="Invalid username or password")
+            return render_template("loginPage.html", error="Invalid User")
     else:
         return render_template("loginPage.html")
  
@@ -61,51 +59,13 @@ def register_controller():
                 return "There was an issue adding your profile"
     else:
         return render_template("register.html")
- 
- 
-# @app.route("/login/", methods=["GET", "POST"])
-# def login_controller():
-#     if request.method == "POST":
-#         username = request.form['username']
-#         password = request.form['password']
-#         user = userChatter.query.filter_by(username=username).first()
-#         if user and user.password == password:
-#             session['username'] = username  
-#             return redirect(url_for("profile", username=username))
-#         else:
-#             return render_template("loginPage.html", error="Invalid username or password")
-#     else:
-#         return render_template("loginPage.html")
- 
-# @app.route("/register/", methods=["GET", "POST"]) 
-# def register_controller():
-#     # Check if the request method is a POST and if so add to database
-#     if request.method == "POST":
-#         username_ = request.form["username"]
-#         email_ = request.form["email"]
-#         password_ = request.form["password"]
-#         repassword_ = request.form["repassword"]
-#         if password_ == repassword_:
-#             try:
-#                 userChatterInfo = userChatter(username=username_, email=email_, password=password_)
-#                 db.session.add(userChatterInfo)
-#                 db.session.commit()
-#                 return redirect(url_for("profile", username=username_))
-#             except:
-#                 return "There was an issue adding your profile"
-#     else:
-#         return render_template("register.html")
+
 
 @app.route("/profile/<username>")
 def profile(username=None):
-    if username:
-        user = userChatter.query.filter_by(username=username).first()
-        chats = chatInfo.query.order_by(chatInfo.date_created.desc()).all()
-        if user:
-            return render_template("chat_page.html", user=user, chatMessage=chats)
-        else:
-            return "specified user not found"
-
+    chats = chatInfo.query.order_by(chatInfo.date_created.desc()).all()
+    user = userChatter.query.filter_by(username=username).first()
+    return render_template('chat_page.html', user=user, chats=chats)
 
 @app.route("/logout/")
 def unlogger():
@@ -118,12 +78,32 @@ def unlogger():
 		return redirect(url_for("login_controller"))
 
 
-# @app.route("/new_message/", methods=["POST"])
-# def new_message():
 
-
-# @app.route("/messages/")
-# def messages():
+@app.route("/new_message/", methods=["POST"]) 
+def new_message(): 
+	message = request.form.get('message')
+	author = request.form.get('author')
+	new_chat = chatInfo(author=author, message=message)
+	try:
+		db.session.add(new_chat)
+		db.session.commit()
+		addChat = {'author': author, 'message': message}
+		return json.dumps(addChat)
+		# return redirect(url_for('profile', username=author))
+	except Exception as e:
+		print(e)
+		return 'There was an error adding your chat message'
+ 
+@app.route("/messages/") 
+def messages():
+	all_chats = chatInfo.query.order_by(chatInfo.date_created.desc()).all()
+	all_chats_json = { }
+	for index, element in enumerate(all_chats):
+		all_chats_json[index] = { }
+		all_chats_json[index]['author'] = element.author
+		all_chats_json[index]['message'] = element.message
+		all_chats_json[index]['datetime'] = element.date_created.date()
+	return jsonify(all_chats_json)
 
 if __name__ == "__main__":
     app.secret_key = "super secret key"
--- static/js/chatPage.js
+++ static/js/chatPage.js
@@ -1,32 +1,51 @@
-const fetchMessages = () => {
-    fetch("/messages/")
-        .then((response) => {
-            return response.json();
-        })
+var messages = "";
+var chat_window;
+
+function setup() {
+    chat_window = document.getElementById("chat_window");
+    getMessages()
+    document.getElementById("new_message_submit").addEventListener("click", newMessage);
+}
+
+function newMessage() {
+    let message = document.getElementById('message').value;
+    let author = document.getElementById('usernameForAjax').innerText;
+    author = author.substring(author.indexOf(':') + 2);
+    fetch("/new_message/", {
+        method: "POST",
+        headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
+        body: `message=${message}&author=${author}`
+    })
+        .then((response) => response.json())
         .then((results) => {
-            let chat_window = document.getElementById("chat_window");
-            let messages = "";
-            for (let index in results) {
-                current_set = results[index];
-                for (let key in current_set) {
-                    author = key;
-                    message = current_set[key];
-                    messages += `${author}:\n${message}\n\n`;
-                }
+            let current_set = results[0];
+            let temp = `<p class="fw-bold"> ${current_set['author']}: ${current_set['message']}</p><hr>`;
+            chat_window.innerHTML += temp;
+        })
+        .catch((e) => {
+            console.log('Error getting the new message: ', e)
+        })
+}
+
+
+function getMessages() {
+    fetch("/messages/")
+        .then((response) => response.json())
+        .then((results) => {
+            var tempMessages = "";
+            for (index in results) {
+                let current_set = results[index];
+                tempMessages += `<p class="fw-bold"> ${current_set['author']}: ${current_set['message']}</p><hr>`;
             }
-            chat_window.value = messages;
+            if (tempMessages !== messages) {
+                messages = tempMessages;
+                chat_window.innerHTML = messages;
+            }
         })
         .catch(() => {
             chat_window.value = "error retrieving messages from server";
-        });
-}
+        })
+    setTimeout(getMessages, 200);
+};
 
-const newMessgae = () => {
-    fetch("/new_message/", {
-        method: "post",
-        headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
-        body: `username=${author}&message=${message}`
-    })
-
-}
-
+window.addEventListener('load', setup);
